ef1b88cd71bf20f567591df1df3e7b52ecabe2e3
Kyle Drake <blazehavenservers@gmail.com>
2021-02-04 22:48:03 -0700

    Optimized script

--- install.sh
+++ install.sh
@@ -2,7 +2,33 @@
 
 set -e
 
-SCRIPT_VERSION="v1.0"
+#############################################################################
+#                                                                           #
+# Project 'pterodactyl-installer'                                           #
+#                                                                           #
+# Copyright (C) 2018 - 2021, Vilhelm Prytz, <vilhelm@prytznet.se>           #
+#                                                                           #
+#   This program is free software: you can redistribute it and/or modify    #
+#   it under the terms of the GNU General Public License as published by    #
+#   the Free Software Foundation, either version 3 of the License, or       #
+#   (at your option) any later version.                                     #
+#                                                                           #
+#   This program is distributed in the hope that it will be useful,         #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of          #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           #
+#   GNU General Public License for more details.                            #
+#                                                                           #
+#   You should have received a copy of the GNU General Public License       #
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.  #
+#                                                                           #
+# https://github.com/vilhelmprytz/pterodactyl-installer/blob/master/LICENSE #
+#                                                                           #
+# This script is not associated with the official Pterodactyl Project.      #
+# https://github.com/vilhelmprytz/pterodactyl-installer                     #
+#                                                                           #
+#############################################################################
+
+SCRIPT_VERSION="v0.2.0"
 
 # exit with error status code if user is not root
 if [[ $EUID -ne 0 ]]; then
@@ -34,34 +60,20 @@ done=false
 
 output "Pterodactyl installation script @ $SCRIPT_VERSION"
 output
-output "https://github.com/icedmoca/pterodactylinstallscript"
+output "Copyright (C) 2018 - 2021, Vilhelm Prytz, <vilhelm@prytznet.se>"
+output "https://github.com/vilhelmprytz/pterodactyl-installer"
 output
-output "This script is not associated with the official Pterodactyl Project or other scripts."
+output "Sponsoring/Donations: https://github.com/vilhelmprytz/pterodactyl-installer?sponsor=1"
+output "This script is not associated with the official Pterodactyl Project."
 
 output
 
 panel() {
-  bash <(curl -s https://raw.githubusercontent.com/icedmoca/pterodactylinstallscript/$SCRIPT_VERSION/install-panel.sh)
+  bash <(curl -s https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/$SCRIPT_VERSION/install-panel.sh)
 }
 
 wings() {
-  bash <(curl -s https://raw.githubusercontent.com/icedmoca/pterodactylinstallscript/$SCRIPT_VERSION/install-wings.sh)
-}
-
-legacy_panel() {
-  bash <(curl -s https://raw.githubusercontent.com/icedmoca/pterodactylinstallscript/$SCRIPT_VERSION/legacy/panel_0.7.sh)
-}
-
-legacy_wings() {
-  bash <(curl -s https://raw.githubusercontent.com/icedmoca/pterodactylinstallscript/$SCRIPT_VERSION/legacy/daemon_0.6.sh)
-}
-
-canary_panel() {
-  bash <(curl -s https://raw.githubusercontent.com/icedmoca/pterodactylinstallscript/master/install-panel.sh)
-}
-
-canary_wings() {
-  bash <(curl -s https://raw.githubusercontent.com/icedmoca/pterodactylinstallscript/master/install-wings.sh)
+  bash <(curl -s https://raw.githubusercontent.com/vilhelmprytz/pterodactyl-installer/$SCRIPT_VERSION/install-wings.sh)
 }
 
 while [ "$done" == false ]; do
@@ -69,31 +81,15 @@ while [ "$done" == false ]; do
     "Install the panel"
     "Install Wings"
     "Install both [0] and [1] on the same machine (wings script runs after panel)\n"
-
-    "Install 0.7 version of panel (unsupported, no longer maintained!)"
-    "Install 0.6 version of daemon (works with panel 0.7, no longer maintained!)"
-    "Install both [3] and [4] on the same machine (daemon script runs after panel)\n"
-
-    "Install panel with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install Wings with canary version of the script (the versions that lives in master, may be broken!)"
-    "Install both [5] and [6] on the same machine (wings script runs after panel)"
   )
 
   actions=(
     "panel"
     "wings"
     "panel; wings"
-
-    "legacy_panel"
-    "legacy_wings"
-    "legacy_panel; legacy_wings"
-
-    "canary_panel"
-    "canary_wings"
-    "canary_panel; canary_wings"
   )
 
-  output "What would you like to do?"
+  output "Pick an option:"
 
   for i in "${!options[@]}"; do
     output "[$i] ${options[$i]}"
